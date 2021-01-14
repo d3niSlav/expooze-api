42bedb1f6f85844ce74860ceceec997956ac8881
Denislav Enchev <denislav.enchev@scalefocus.com>
2021-01-14 21:43:24 +0200

    Added change password route.

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 
+import { UserService } from './user.service';
+import ChangePasswordDto from '../auth/dto/change-password.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @UseGuards(JwtAuthGuard)
   @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
+  getProfile(@Request() request) {
+    return request.user;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('change-password')
+  async changePassword(
+    @Request() request,
+    @Body() changePasswordDto: ChangePasswordDto,
+  ) {
+    await this.userService.changeUserPassword({
+      id: request.user.id,
+      password: changePasswordDto.password,
+    });
   }
 }
