3738b216cae5bf44af805e3418e8eeaa35c8a212
Denislav Enchev <denislav.enchev@scalefocus.com>
2021-02-23 17:06:04 +0200

    Added CORS library

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ValidationError } from 'class-validator';
+import cors from 'cors';
 
 import { BadRequestExceptionFilter } from './exceptions/bad-request.filter';
 import { mapErrorMessagesFromValidator } from './exceptions/exceptions.helpers';
@@ -19,7 +20,11 @@ const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: true });
 
-  app.enableCors();
+  app.use(
+    cors({
+      exposedHeaders: ['Content-Disposition'],
+    }),
+  );
 
   app.useGlobalFilters(
     new HttpExceptionFilter(),
